b813d7a3c3ff65d9bd3d946ffc6e81349f7cf046
averywlittle <averywlittle@gmail.com>
2021-02-14 21:48:51 -0500

    some endpoints set up that take in a city name and print the data from the weather map api

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
+const fetch = require('node-fetch')
 const app = express()
 
 app.use(cors())
@@ -10,7 +11,29 @@ app.use(express.json())
 
 // Routes
 
+app.post('/api/current/', (request, response, next) => {
+    
+    const cityName = request.body.cityName
+    const reqUrl = `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${process.env.API_KEY}`
 
+    fetch(reqUrl)
+        .then(res => res.json())
+        .then(body => console.log(body));
+
+})
+
+app.post('/api/5day/', (request, response, next) => {
+    
+    // validate incoming data
+    const cityName = request.body.cityName
+    const reqUrl = `http://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${process.env.API_KEY}`
+
+    fetch(reqUrl)
+        .then(res => res.json())
+        .then(body => console.log(body));
+
+    // return data
+})
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
